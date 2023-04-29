3b3f1df2b075a03e390c88da285a756be666e1b2
lat9 <lat9@vinosdefrutastropicales.com>
2023-04-29 17:10:59 -0400

    #5: Using zc158 language structure ...
    ... and removing now unused constants.

---
+++ includes/languages/english/YOUR_TEMPLATE/lang.product_reviews_write.php
@@ -0,0 +1,24 @@
+<?php
+// -----
+// Part of the "Reviews Updated" plugin by lat9
+// Copyright (C) 2023, Vinos de Frutas Tropicales
+//
+// Last updated: v2.0.0
+//
+// -----
+// If the plugin's processing is not enabled, no language overrides needed.
+//
+if (!isset($GLOBALS['guest_reviews_enabled'])) {
+    return [];
+}
+
+// -----
+// Otherwise, provide the override for the SUB_TITLE_REVIEW definition and add the
+// couple of guest-related language definitions.
+//
+$defines = [
+    'SUB_TITLE_REVIEW' => 'Your name will be displayed as the text you enter in <em>Your Name</em>. For your privacy, we suggest <b>not</b> using your full name.',
+    'JS_REVIEW_NAME' => 'Your Name needs to have at least ' . REVIEW_NAME_MIN_LENGTH . ' characters.',
+    'TEXT_REVIEW_NAME' => 'Your Name:',
+}
+return $defines;
